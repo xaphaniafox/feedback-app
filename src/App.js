60d5a4d205aaa00cfcf2e5beecea2eb0ab122249
xaphaniafox <112392902+xaphaniafox@users.noreply.github.com>
2022-11-22 06:50:06 +0330

    a little improvment in Comments ul

--- src/App.js
+++ src/App.js
@@ -12,12 +12,60 @@ function App() {
       <p>{ body }</p>
       <h3>Comments ({ comments.length })</h3>
       <ul>
-        {comments.map((comment, index) => (
-          <li>{ comment.text }</li>
-        ))}
+        { comments.map((comment, index) => {
+          return <li key={ index }>{ comment.text }</li>
+        }) }
       </ul>
     </div>
   )
 }
 
 export default App;
+
+// function App() {
+//   const title = 'Blog Post';
+//   const body = 'This is my blog post';
+//   const comments = [
+//     {id: 1, text: 'Comment One'},
+//     {id: 2, text: 'Comment Two'},
+//     {id: 3, text: 'Comment Three'},
+//   ]
+//   return (
+//     <div>
+//       <h1>{ title }</h1>
+//       <p>{ body }</p>
+//       <h3>Comments ({ comments.length })</h3>
+//       <ul>
+//         {comments.map((comment, index) => {
+//           return <li key={index}>{ comment.text }</li>
+//         })}
+//       </ul>
+//     </div>
+//   )
+// }
+
+// export default App;
+
+// function App() {
+//   const title = 'Blog Post';
+//   const body = 'This is my blog post';
+//   const comments = [
+//     {id: 1, text: 'Comment One'},
+//     {id: 2, text: 'Comment Two'},
+//     {id: 3, text: 'Comment Three'}
+//   ]
+//   return (
+//     <div className='container'>
+//       <h1>{ title.toUpperCase() }</h1>
+//       <p>{ body }</p>
+//       <h3>Comments ({ comments.length })</h3>
+//       <ul>
+//         {comments.map((comment, index) => (
+//           <li>{ comment.text }</li>
+//         ))}
+//       </ul>
+//     </div>
+//   )
+// }
+
+// export default App;
