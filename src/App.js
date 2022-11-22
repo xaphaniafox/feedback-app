a78ce53eab54f8bd713d967027c7a3108b71ea2d
xaphaniafox <112392902+xaphaniafox@users.noreply.github.com>
2022-11-22 05:38:52 +0330

    initializing the project, deleting the default files and adding new ones

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
+  const title = 'Blog Post';
+  const body = 'This is my blog post';
+  const comments = [
+    {id: 1, text: 'Comment One'},
+    {id: 2, text: 'Comment Two'},
+    {id: 3, text: 'Comment Three'}
+  ]
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container'>
+      <h1>{ title.toUpperCase() }</h1>
+      <p>{ body }</p>
+      <h3>Comments ({ comments.length })</h3>
+      <ul>
+        {comments.map((comment, index) => (
+          <li>{ comment.text }</li>
+        ))}
+      </ul>
     </div>
-  );
+  )
 }
 
 export default App;
