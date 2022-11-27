f94562b9d89ab42abf1995742c30427992fd2e56
xaphaniafox <112392902+xaphaniafox@users.noreply.github.com>
2022-11-27 21:08:43 +0330

    the end of section 2. React Basics & JSx

--- src/App.js
+++ src/App.js
@@ -1,71 +1,8 @@
 function App() {
-  const title = 'Blog Post';
-  const body = 'This is my blog post';
-  const comments = [
-    {id: 1, text: 'Comment One'},
-    {id: 2, text: 'Comment Two'},
-    {id: 3, text: 'Comment Three'}
-  ]
   return (
-    <div className='container'>
-      <h1>{ title.toUpperCase() }</h1>
-      <p>{ body }</p>
-      <h3>Comments ({ comments.length })</h3>
-      <ul>
-        { comments.map((comment, index) => {
-          return <li key={ index }>{ comment.text }</li>
-        }) }
-      </ul>
+    <div className="container">
+      <h1>My App</h1>
     </div>
   )
 }
-
 export default App;
-
-// function App() {
-//   const title = 'Blog Post';
-//   const body = 'This is my blog post';
-//   const comments = [
-//     {id: 1, text: 'Comment One'},
-//     {id: 2, text: 'Comment Two'},
-//     {id: 3, text: 'Comment Three'},
-//   ]
-//   return (
-//     <div>
-//       <h1>{ title }</h1>
-//       <p>{ body }</p>
-//       <h3>Comments ({ comments.length })</h3>
-//       <ul>
-//         {comments.map((comment, index) => {
-//           return <li key={index}>{ comment.text }</li>
-//         })}
-//       </ul>
-//     </div>
-//   )
-// }
-
-// export default App;
-
-// function App() {
-//   const title = 'Blog Post';
-//   const body = 'This is my blog post';
-//   const comments = [
-//     {id: 1, text: 'Comment One'},
-//     {id: 2, text: 'Comment Two'},
-//     {id: 3, text: 'Comment Three'}
-//   ]
-//   return (
-//     <div className='container'>
-//       <h1>{ title.toUpperCase() }</h1>
-//       <p>{ body }</p>
-//       <h3>Comments ({ comments.length })</h3>
-//       <ul>
-//         {comments.map((comment, index) => (
-//           <li>{ comment.text }</li>
-//         ))}
-//       </ul>
-//     </div>
-//   )
-// }
-
-// export default App;
