8f8902daa0aa468282b95dc9e903ad6bd55ba3f7
xaphaniafox <112392902+xaphaniafox@users.noreply.github.com>
2022-11-29 06:43:32 +0330

    the end of Custom Button Component section

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import Card from "./shared/Card";
+import Button from "./shared/Button";
 
 function FeedbackForm() {
   const [text, setText] = useState('');
@@ -16,7 +17,7 @@ function FeedbackForm() {
             type="text" 
             placeholder='Write a review'
             value={ text } />
-          <button type="submit">Send</button>
+          <Button type="submit">Send</Button>
         </div>
       </form>
     </Card>
