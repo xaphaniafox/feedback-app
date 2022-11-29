4d5fe86d978afe94528aa04ba18c035eca6f92f1
xaphaniafox <112392902+xaphaniafox@users.noreply.github.com>
2022-11-29 23:12:39 +0330

    the end of Add Feedback section

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { v4 as uuidv4} from 'uuid';
 import { useState } from "react";
 import Header from "./components/Header";
 import FeedbackList from "./components/FeedbackList";
@@ -6,16 +7,23 @@ import FeedbackForm from "./components/FeedbackForm";
 import FeedbackData from "./data/FeedbackData";
 
 function App() {
-  const [feedback, setFeedback] = useState(FeedbackData)
+  const [feedback, setFeedback] = useState(FeedbackData);
+
+  const addFeedback = (newFeedback) => {
+    newFeedback.id = uuidv4();
+    setFeedback([newFeedback, ...feedback])
+  }
+
   const deleteFeedback = (id) => {
     if (window.confirm('Are you sure you want to delete?'))
       setFeedback(feedback.filter((item) => item.id !== id))
   }
+
   return (
     <>
       <Header />
       <div className="container">
-        <FeedbackForm />
+        <FeedbackForm handleAdd={ addFeedback } />
         <FeedbackStats feedback={ feedback } />
         <FeedbackList feedback={ feedback } handleDelete={ deleteFeedback } />
       </div>
