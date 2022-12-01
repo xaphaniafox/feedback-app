b0dd1c3a9c99baf026699defef0ba3fce83aa91a
xaphaniafox <112392902+xaphaniafox@users.noreply.github.com>
2022-12-01 23:39:05 +0330

    putting content in about page

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,8 +1,15 @@
+import Card from "../components/shared/Card";
+
 function AboutPage() {
   return (
-    <div>
-      About Page
-    </div>
+    <Card>
+      <div className="about">
+        <h1>About this project</h1>
+        <p>This is a React app to leave feedback for a product or service</p>
+        <p>version 1.0.0</p>
+        <p><a href='/'>Back to home</a></p>
+      </div>
+    </Card>
   )
 }
 
