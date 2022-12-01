7db72fda939df2b25f516247dc0b6ace02bf3158
xaphaniafox <112392902+xaphaniafox@users.noreply.github.com>
2022-12-01 20:38:47 +0330

    just changing the default selected rating number

--- src/components/RatingSelect.jsx
+++ src/components/RatingSelect.jsx
@@ -1,6 +1,6 @@
 import{ useState} from 'react';
 function RatingSelect({select}) {
-  const [selected, setSelected] = useState(4);
+  const [selected, setSelected] = useState(10);
   const handleChange = (e) => {
     setSelected(+e.currentTarget.value);
     select(+e.currentTarget.value);
