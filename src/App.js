e044b915ff8911843aff5c78a11b28cd8212e922
xaphaniafox <f.hosseini64@gmail.com>
2023-01-20 19:59:57 +0300

    creating Context Provider (createContext)

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import FeedbackForm from "./components/FeedbackForm";
 import FeedbackData from "./data/FeedbackData";
 import AboutPage from './pages/AboutPage';
 import AboutIconLink from './components/AboutIconLink';
+import { FeedbackProvider } from './components/context/FeedbackContext';
 
 function App() {
   const [feedback, setFeedback] = useState(FeedbackData);
@@ -23,23 +24,25 @@ function App() {
   }
 
   return (
-    <Router>
-      <Header />
-      <div className="container">
-        <Routes>
-          <Route exact path='/' element={
-            <>
-              <FeedbackForm handleAdd={ addFeedback } />
-              <FeedbackStats feedback={ feedback } />
-              <FeedbackList feedback={ feedback } handleDelete={ deleteFeedback } />
-            </>
-          }>
-          </Route>
-          <Route path='/about' element={<AboutPage />} />
-        </Routes>
-        <AboutIconLink /> 
-      </div>
-    </Router>
+    <FeedbackProvider>
+      <Router>
+        <Header />
+        <div className="container">
+          <Routes>
+            <Route exact path='/' element={
+              <>
+                <FeedbackForm handleAdd={ addFeedback } />
+                <FeedbackStats feedback={ feedback } />
+                <FeedbackList feedback={ feedback } handleDelete={ deleteFeedback } />
+              </>
+            }>
+            </Route>
+            <Route path='/about' element={<AboutPage />} />
+          </Routes>
+          <AboutIconLink /> 
+        </div>
+      </Router>
+    </FeedbackProvider>
   )
 }
 export default App;
