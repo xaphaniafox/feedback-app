1f4a13a47118b5e7c2e6fc70ac6d5987e5575e8a
xaphaniafox <f.hosseini64@gmail.com>
2023-01-23 07:52:37 +0300

    changing the title:

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -15,7 +15,7 @@ function Header({ text, bgColor, textColor }) {
 }
 
 Header.defaultProps = {
-  text : 'Feedback UI',
+  text : 'your FEEDBACK, share with us',
   bgColor : 'rgba(0,0,0,0.4)',
   textColor : '#ff6a95',
 }
